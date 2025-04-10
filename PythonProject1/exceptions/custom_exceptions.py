9fec26de9ec92f367054e3df2e627cc148ceeb0d
Reshhma3 <reshhmareceskct@gmail.com>
2025-04-10 16:06:00 +0530

    Delete PythonProject1 directory

--- PythonProject1/exceptions/custom_exceptions.py
+++
@@ -1,6 +0,0 @@
-class InvalidDataException(Exception): pass
-class InsufficientStockException(Exception): pass
-class IncompleteOrderException(Exception): pass
-class PaymentFailedException(Exception): pass
-class AuthenticationException(Exception): pass
-class AuthorizationException(Exception): pass
